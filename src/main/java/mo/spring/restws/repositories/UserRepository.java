6e5dfce9cf10c49d01c603fd6ec7e9e4df779602
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-10 15:04:19 +0100

    81 - Executer une Requete SQL Native

--- src/main/java/mo/spring/restws/repositories/UserRepository.java
+++ src/main/java/mo/spring/restws/repositories/UserRepository.java
@@ -1,5 +1,8 @@
 package mo.spring.restws.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,7 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
 	
 	UserEntity findByEmail(String email);
 	UserEntity findByUserId(String userId);
+	
+	@Query(value = "select * from users u where u.firstname = 'Moussa'", nativeQuery = true)
+	Page<UserEntity> findAllUserByFirstName(Pageable pageableRequest);
 }
