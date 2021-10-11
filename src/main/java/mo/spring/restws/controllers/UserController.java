1be7b0b70eccc962e3d77d8869819e6aa9887437
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-11 01:36:29 +0100

    89 - C_est quoi le CORS (Cross Origin Resource Sharing)

--- src/main/java/mo/spring/restws/controllers/UserController.java
+++ src/main/java/mo/spring/restws/controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import mo.spring.restws.responses.UserResponse;
 import mo.spring.restws.services.UserService;
 import mo.spring.restws.shared.dto.UserDto;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -46,7 +48,8 @@ public class UserController {
 		
 		return new ResponseEntity<>(userResponse, HttpStatus.OK);
 	}
-	
+
+	@CrossOrigin(origins = {"http://localhost:4200"})
 	@GetMapping
 	public ResponseEntity<List<UserResponse>> getAllUsers(@RequestParam(value="page", defaultValue="1") int page, @RequestParam(value="limit", defaultValue="15") int limit) {
 		List<UserResponse> usersResponse = new ArrayList<>();
