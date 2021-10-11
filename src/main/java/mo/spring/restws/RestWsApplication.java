b5d6d8609e696473e88c558a8e257ef1ac22b8bc
Moussa OUSTOUH <moussa.oustouh@gmail.com>
2021-10-11 02:36:25 +0100

    95 - Livrable WAR Archive

--- src/main/java/mo/spring/restws/RestWsApplication.java
+++ src/main/java/mo/spring/restws/RestWsApplication.java
@@ -2,11 +2,18 @@ package mo.spring.restws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class RestWsApplication {
+public class RestWsApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RestWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestWsApplication.class, args);
